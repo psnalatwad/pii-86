05073effc68b5104393d94ee078717c887c65de8
unknown <you@example.com>
2025-05-15 00:18:22 -0700

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello maven
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
